52d07d186d59a42abb09fb45c70df427a0cb21df
wolframhwang <hzw94@naver.com>
2022-06-27 00:25:59 +0900

    [feat] TodoList Cell 추가 반영

--- DidIDoMyBestSU/View/TodoListView.swift
+++ DidIDoMyBestSU/View/TodoListView.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 struct TodoListView: View {
+    @EnvironmentObject var manager: CoreDataManager
+    
+    @FetchRequest(sortDescriptors: [SortDescriptor(\TaskEntity.insertDate, order: .reverse)])
+    var taskList: FetchedResults<TaskEntity>
+    
     var body: some View {
         NavigationView {
             List {
-                
+                ForEach(taskList) { task in
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("\(task.title ?? "")")
+                    }
+                }
             }
         }
     }
