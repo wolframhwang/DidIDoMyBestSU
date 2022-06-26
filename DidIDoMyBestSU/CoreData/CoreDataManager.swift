4f6c40130aadc608f24a491ef7b5bb45274de2c2
wolframhwang <hzw94@naver.com>
2022-06-26 23:07:55 +0900

    [feat] CoreData: 코어 데이터 부분 추가사항 반영

--- DidIDoMyBestSU/CoreData/CoreDataManager.swift
+++ DidIDoMyBestSU/CoreData/CoreDataManager.swift
@@ -10,5 +10,55 @@ import CoreData
 import SwiftUI
 
 class CoreDataManager: ObservableObject {
+    static let shared = CoreDataManager()
     
+    let container: NSPersistentContainer
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+        
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "SwiftUIMemo")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func saveContext() {
+        if mainContext.hasChanges {
+            do {
+                try mainContext.save()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func addMemo(content: String) {
+        let newMemo = TaskEntity(context: mainContext)
+        newMemo.content = content
+        newMemo.insertDate = Date.now
+        
+        saveContext()
+    }
+    
+    
+    func update(memo: TaskEntity?, content: String) {
+        memo?.content = content
+        saveContext()
+    }
+    
+    func delete(memo: TaskEntity?) {
+        if let memo = memo {
+            mainContext.delete(memo)
+            saveContext()
+        }
+    }
 }
